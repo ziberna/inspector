95f9908c822eedc5af18ee5c02ad0d32d8a6f631
Jure Ziberna <j.ziberna@gmail.com>
2012-02-18 19:21:57 +0100

    packaging update

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     author='Jure Ziberna and Andrew Moffat (Inspect-Shell)',
     author_email='j.ziberna@gmail.com',
     url='https://github.com/jzib/inspector',
-    version='0.5.0',
+    version='0.5.1',
     license='GNU GPL 3',
     py_modules=['inspector']
 )
