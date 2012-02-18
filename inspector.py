62de72419c0b2d1c8247ef575d47271624408f26
Jure Ziberna <j.ziberna@gmail.com>
2012-02-18 19:21:32 +0100

    fix for empty response

--- inspector.py
+++ inspector.py
@@ -69,7 +69,7 @@ else:
 import argparse
 
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __copyright__ = """Copyright (C) 2011 by Andrew Moffat
 Copyright (C) 2012  Jure Ziberna"""
 __license__ = 'GNU GPL 3'
@@ -155,6 +155,8 @@ class Socket(object):
         """
         header = self.socket.recv(self.chunk_size)
         length, message = self.parse_header(header)
+        if not length:
+            return None
         while len(message) < length:
             message += self.socket.recv(self.chunk_size)
         return json.loads(message.decode())
@@ -166,7 +168,10 @@ class Socket(object):
         """
         header_separator = self.header_separator.encode()
         length, separator, message_chunk = header.partition(header_separator)
-        return int(length), message_chunk
+        try:
+            return int(length), message_chunk
+        except ValueError:
+            return None, None
 
 
 class ImporterServer(object):
